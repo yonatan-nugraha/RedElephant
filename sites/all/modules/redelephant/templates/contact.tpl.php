974eb44de81610a9f7978b7a994cd973e5e57465
Poppy Gantini <rpoppy@icloud.com>
2015-01-01 10:05:18 +0700

    update contact & news page

--- sites/all/modules/redelephant/templates/contact.tpl.php
+++ sites/all/modules/redelephant/templates/contact.tpl.php
@@ -2,39 +2,38 @@
 document.getElementById("contact").classList.add("active");
 </script>
 
-<div class="row" style="margin-top: 30px">
-  <div class="col-xs-4">
-    <strong style="margin-bottom: 10px; font-size: 42px; color: red">HELLO THERE!</strong>
-  </div>
-</div>
-
-<div class="row" style="margin-bottom: 20px">
-  <div class="col-xs-4">
-    <strong>Please take a moment to let us know what brought you here - we'd love to hear more about you</strong>
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-xs-4">
-		<form role="form" id="myForm" action="admin/red-elephant/contact/submit" method="post" onsubmit="return alert('Thank you for taking your time to fill this form!');">
-		  <div class="form-group">
-		    <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
-		  </div>
-		  <div class="form-group">
-		    <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
-		  </div>
-		  <div class="form-group">
-		    <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" required>
-		  </div>
-		  <div class="form-group">
-		  	<textarea class="form-control" rows="5" id="address" name="address" placeholder="Address" required></textarea>
-		  </div>
-		  <button type="submit" class="btn btn-default pull-right">Submit</button>
-		</form>
-  </div>
-  <div class="col-xs-1" style="min-height: 300px; border-right: solid 2px">
-  </div>
-	<div class="col-xs-6" style="margin-left: 15px">
-		<strong>PT. REPRO KONSULTAN INDONESIA<br>Epicentrum Walk 6th FI, unit #B627<br>Jl. HR Rasuna Said | Jakarta 12940<br>Indonesia<br><br>p. +6221 299 121 43<br>f. +6221 299 121 42</strong>
-  </div>
+<div class="contact-us">
+	<h2>HELLO THERE!</h2>
+	<p>Please take a moment to let us know what are your thoughts -</p>
+	<div class="row contact-form">
+		<div class="col-lg-5">
+			<div class="row">
+				<div class="col-lg-10">
+					<form role="form" id="myForm" action="admin/red-elephant/contact/submit" method="post" onsubmit="return alert('Thank you for taking your time to fill this form!');">
+						<div class="form-group">
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+						</div>
+						<div class="form-group">
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" required>
+						</div>
+						<div class="form-group">
+							<textarea class="form-control" rows="5" id="address" name="address" placeholder="Note" required></textarea>
+						</div>
+						<button type="submit" class="btn btn-default pull-right">Submit</button>
+					</form>
+				</div>
+				<div class="col-lg-2">&nbsp;</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="col-lg-7 address">
+			<h4 class="logo">Red Elephant</h4>
+			<p>PT. REPRO KONSULTAN INDONESIA<br>Epicentrum Walk 6th Fl, unit #B627<br>Jl. HR Rasuna Said | Jakarta 12940<br>Indonesia</p>
+			<p>p. +62 21 299 121 43 | f. +62 21 299 121 42</p>
+		</div>
+		<div class="clearfix"></div>
+	</div>
 </div>
--- sites/all/modules/redelephant/templates/news.tpl.php
+++ sites/all/modules/redelephant/templates/news.tpl.php
@@ -3,27 +3,27 @@ document.getElementById("news").classList.add("active");
 </script>
 
 <div class="row">
-
   <div class="col-lg-3 news-title">
  		<h2>WHAT'S NEW?</h2>
   </div>
 
   <div class="col-lg-9">
   	<?php foreach($news as $i => $n) { ?>
-  	<div class="row news-item" onclick="location.href='news/<?php print $n->id; ?>';">
-  		<div class="col-lg-5">
-          <img class="img-responsive" src="<?php print file_create_url('public://') . $n->filename; ?>">
-  		</div>
-  		<div class="col-lg-7">
-        <span class="date"><?php print $n->date; ?></span>
-        <h4><?php print $n->title; ?></h4>
-        <div class="description">
-          <?php print $n->description; ?>
+  	<div class="news-item" onclick="location.href='news/<?php print $n->id; ?>';">
+      <div class="row">
+        <div class="col-lg-5">
+            <img class="img-responsive" src="<?php print file_create_url('public://') . $n->filename; ?>">
         </div>
-        <p>>></p>
+        <div class="col-lg-7">
+          <span class="date"><?php print $n->date; ?></span>
+          <h4><?php print $n->title; ?></h4>
+          <div class="description">
+            <?php print $n->description; ?>
+          </div>
+          <p>>></p>
+        </div>
       </div>
   	</div>
   	<?php } ?>
 	</div>
-
 </div>
