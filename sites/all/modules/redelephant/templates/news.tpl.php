bfd12e694edc5d82c7021f68380426fe581acb45
Yonatan Nugraha <yonatan.nugraha@hotmail.com>
2015-01-26 08:41:44 +0700

    fix several things

--- sites/all/modules/redelephant/templates/news.tpl.php
+++ sites/all/modules/redelephant/templates/news.tpl.php
@@ -8,11 +8,11 @@ document.getElementById("news").classList.add("active");
   </div>
 
   <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-  	<?php foreach($news as $i => $n) { ?>
+  	<?php foreach($news as $i => $n) { $filename = explode("/", $n->filename); ?>
   	<div class="news-item" onclick="location.href='news/<?php print $n->id; ?>';">
       <div class="row">
         <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-            <img class="img-responsive" src="<?php print file_create_url('public://') . $n->filename; ?>">
+            <img class="img-responsive" src="<?php print file_create_url('public://') . $filename[0]; ?>">
         </div>
         <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
           <span class="date"><?php print $n->date; ?></span>
--- sites/all/modules/redelephant/templates/newsdetail.tpl.php
+++ sites/all/modules/redelephant/templates/newsdetail.tpl.php
@@ -1,12 +1,22 @@
 <script>
 document.getElementById("news").classList.add("active");
+
+function set_image(id, filepath) {
+  document.getElementById(id).src = filepath;
+}
 </script>
 
 <div class="news-detail">
   <h2>WHAT'S NEW?</h2>
   <div class="row">
+    <?php $filename = explode("/", $news->filename); ?>
 	  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-      <img class="img-responsive" src="<?php print file_create_url('public://') . $news->filename; ?>">
+      <img id="img-newsdetail" class="img-responsive" src="<?php print file_create_url('public://') . $filename[0]; ?>">
+      <?php for ($i=0; $i<3; $i++) { if (isset($filename[$i])) { ?>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <img class="img-responsive" src="<?php print file_create_url('public://') . $filename[$i]; ?>" onclick="set_image('<?php print "img-newsdetail" . "','" . file_create_url('public://') . $filename[$i] ?>')" style="cursor: pointer">
+      </div>
+      <?php }} ?>
 		</div>
 		<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
 			<span class="date"><?php print $news->date; ?></span>
